19d60cf114ea5424a50b7bb18083bd3f540e4bec
vynnychenko-alex <82096279+vynnychenko-alex@users.noreply.github.com>
2023-05-10 12:06:08 +0300

    Update upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=dimension90/aiapp
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag aiapp:1.0 $dockerpath:1.0
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:1.0
