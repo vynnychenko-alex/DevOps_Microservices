6165d03deae5ca081a3bf9be7722809af949f96c
vynnychenko-alex <82096279+vynnychenko-alex@users.noreply.github.com>
2023-05-10 14:18:07 +0300

    Update run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,20 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=dimension90/aiapp
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl create deployment aiapp-minikube --image=$dockerpath:latest
+
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods --all-namespaces
+
 
 # Step 4:
 # Forward the container port to a host
+kubectl expose deployment aiapp-minikube --type=NodePort --port=80
 
